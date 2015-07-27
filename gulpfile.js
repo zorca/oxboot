1b4b8f4a30a9af626e7179ec1a515eb4152b515c
Steelcat <steelpuppy@gmail.com>
2015-07-27 21:48:00 +0300

    Build tasks added

--- gulpfile.js
+++ gulpfile.js
@@ -12,8 +12,10 @@ var SRC_PATH =          './app',
     SCRIPTS_FOLDER =    'scripts',
     IMAGES_FOLDER =     'images',
 // Options
+    CSS_PREPROCESSOR =  'sass',
     TEMPLATES_EXT =     '.php',
-    DEV_SERVER =        true;
+    DEV_SERVER =        true,
+    DEV_MODE =          false;
 
 /*
  * Gulp plugins
@@ -23,41 +25,48 @@ var del = require('del'),
     connect = require('gulp-connect-php'),
     browsersync = require('browser-sync'),
     reload  = browsersync.reload,
+    plumber = require('gulp-plumber'),
     debug = require('gulp-debug'),
     gulpif = require('gulp-if'),
+    gutil = require('gulp-util'),
     jade = require('gulp-jade'),
     less = require('gulp-less'),
-    plumber = require('gulp-plumber'),
+    scss = require('gulp-sass'),
+    cssmin = require('gulp-minify-css'),
+    notify = require('gulp-notify'),
     templatesglob = require('gulp-jade-globbing'),
     stylesglob = require('gulp-css-globbing'),
     rename = require('gulp-rename');
 
-// PHP server task
-gulp.task('server', function() {
-    connect.server({
-        base: DEV_PATH+'/'+TEMPLATES_FOLDER+'/'+TEMPLATE_NAME, port:8010, keepalive: true
-    });
-});
+// Destination Path
+if (DEV_MODE) {
+    DEST_PATH = DEV_PATH
+} else {
+    DEST_PATH = PUB_PATH
+}
 
-// BrowserSync Task
-gulp.task('browser-sync',['server'], function() {
+// Server task
+gulp.task('server', function() {
     browsersync({
         proxy: '127.0.0.1:8010',
         port: 8080,
         open: true,
         notify: false
     });
+    connect.server({
+        base: DEST_PATH+'/'+TEMPLATES_FOLDER+'/'+TEMPLATE_NAME, port:8010, keepalive: true
+    });
 });
 
-// Clean Dev folder
-gulp.task('clean:dev', function (cb) {
-    del(DEV_PATH+'*', cb);
+// Clean Destination folder
+gulp.task('clean', function (cb) {
+    del(DEST_PATH+'*', cb);
 });
 
-// Jade Dev task
-gulp.task('templates:dev', function() {
+// Jade task
+gulp.task('templates', function() {
     return gulp.src(SRC_PATH+'/'+TEMPLATES_FOLDER+'/'+TEMPLATE_NAME+'/**/[^_]*.jade')
-        .pipe(plumber())
+        .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
         .pipe(templatesglob())
         .pipe(jade({
             pretty: true,
@@ -69,43 +78,68 @@ gulp.task('templates:dev', function() {
         .pipe(rename({
             extname: TEMPLATES_EXT
         }))
-        .pipe(gulp.dest(DEV_PATH+'/'+TEMPLATES_FOLDER+'/'+TEMPLATE_NAME))
+        .pipe(gulp.dest(DEST_PATH+'/'+TEMPLATES_FOLDER+'/'+TEMPLATE_NAME))
 });
 
-// Less Dev task
-gulp.task('styles:dev', function() {
-    return gulp.src(SRC_PATH+'/'+TEMPLATES_FOLDER+'/'+TEMPLATE_NAME+'/'+STYLES_FOLDER+'/**/[^_]*.less')
-        .pipe(stylesglob({
-            extensions: ['.css','.less'],
-            scssImportPath: {
-                leading_underscore: true,
-                filename_extension: false
-            }
-        }))
-        .pipe(less({errLogToConsole: true}))
-        .pipe(gulp.dest(DEV_PATH+'/'+TEMPLATES_FOLDER+'/'+TEMPLATE_NAME+'/'+STYLES_FOLDER));
-});
+switch (CSS_PREPROCESSOR) {
+    case 'sass':
+        // Sass task
+        gulp.task('styles', function() {
+            return gulp.src(SRC_PATH+'/'+TEMPLATES_FOLDER+'/'+TEMPLATE_NAME+'/'+STYLES_FOLDER+'/**/[^_]*.scss')
+                .pipe(stylesglob({
+                    extensions: ['.css','.scss'],
+                    scssImportPath: {
+                        leading_underscore: true,
+                        filename_extension: false
+                    }
+                }))
+                .pipe(scss({errLogToConsole: true}))
+                .pipe(gulpif(!DEV_MODE, cssmin()))
+                .pipe(gulpif(!DEV_MODE, rename({extname: '.min.css'})))
+                .pipe(gulp.dest(DEST_PATH+'/'+TEMPLATES_FOLDER+'/'+TEMPLATE_NAME+'/'+STYLES_FOLDER));
+        });
+    break
+    case 'less':
+        // Less task
+        gulp.task('styles', function() {
+            return gulp.src(SRC_PATH+'/'+TEMPLATES_FOLDER+'/'+TEMPLATE_NAME+'/'+STYLES_FOLDER+'/**/[^_]*.less')
+                .pipe(stylesglob({
+                    extensions: ['.css','.less'],
+                    scssImportPath: {
+                        leading_underscore: true,
+                        filename_extension: false
+                    }
+                }))
+                .pipe(less({errLogToConsole: true}))
+                .pipe(gulpif(!DEV_MODE, cssmin()))
+                .pipe(gulpif(!DEV_MODE, rename({extname: '.min.css'})))
+                .pipe(gulp.dest(DEST_PATH+'/'+TEMPLATES_FOLDER+'/'+TEMPLATE_NAME+'/'+STYLES_FOLDER));
+        });
+    break
+}
 
-// Scripts Dev task
-gulp.task('scripts:dev', function() {
+
+// Scripts task
+gulp.task('scripts', function() {
     return gulp.src([SRC_PATH+'/'+TEMPLATES_FOLDER+'/'+TEMPLATE_NAME+'/'+SCRIPTS_FOLDER+'/**/[^_]*.js'])
-        .pipe(gulp.dest(DEV_PATH+'/'+TEMPLATES_FOLDER+'/'+TEMPLATE_NAME+'/'+SCRIPTS_FOLDER));
+        .pipe(gulp.dest(DEST_PATH+'/'+TEMPLATES_FOLDER+'/'+TEMPLATE_NAME+'/'+SCRIPTS_FOLDER));
 });
 
-// Images Dev task
-gulp.task('images:dev', function() {
+// Images task
+gulp.task('images', function() {
     return gulp.src([SRC_PATH+'/'+TEMPLATES_FOLDER+'/'+TEMPLATE_NAME+'/'+IMAGES_FOLDER+'/**/[^_]*.*'])
-        .pipe(gulp.dest(DEV_PATH+'/'+TEMPLATES_FOLDER+'/'+TEMPLATE_NAME+'/'+IMAGES_FOLDER));
+        .pipe(gulp.dest(DEST_PATH+'/'+TEMPLATES_FOLDER+'/'+TEMPLATE_NAME+'/'+IMAGES_FOLDER));
 });
 
 // Watch tasks
-gulp.task('watch:dev', function() {
-    gulp.watch(SRC_PATH+'/'+TEMPLATES_FOLDER+'/'+TEMPLATE_NAME+'/**/*.jade', ['templates:dev' , reload]);
-    gulp.watch(SRC_PATH+'/'+TEMPLATES_FOLDER+'/'+TEMPLATE_NAME+'/'+STYLES_FOLDER+'/**/*.less', ['styles:dev', reload]);
-    gulp.watch(SRC_PATH+'/'+TEMPLATES_FOLDER+'/'+TEMPLATE_NAME+'/'+SCRIPTS_FOLDER+'/**/*.js', ['scripts:dev', reload]);
+gulp.task('watch', function() {
+    gulp.watch(SRC_PATH+'/'+TEMPLATES_FOLDER+'/'+TEMPLATE_NAME+'/**/*.jade', ['templates' , reload]);
+    gulp.watch(SRC_PATH+'/'+TEMPLATES_FOLDER+'/'+TEMPLATE_NAME+'/'+STYLES_FOLDER+'/**/*.less', ['styles', reload]);
+    gulp.watch(SRC_PATH+'/'+TEMPLATES_FOLDER+'/'+TEMPLATE_NAME+'/'+STYLES_FOLDER+'/**/*.scss', ['styles', reload]);
+    gulp.watch(SRC_PATH+'/'+TEMPLATES_FOLDER+'/'+TEMPLATE_NAME+'/'+SCRIPTS_FOLDER+'/**/*.js', ['scripts', reload]);
 });
 
 // Default task
-gulp.task('default', ['clean:dev'], function() {
-    gulp.start('browser-sync','templates:dev','styles:dev', 'scripts:dev', 'images:dev','watch:dev');
+gulp.task('default', ['clean'], function() {
+    gulp.start('server','templates','styles', 'scripts', 'images','watch');
 });
